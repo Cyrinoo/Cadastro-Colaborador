d913a81515050cc20f6e12d5c32a9f52bf34f85f
Pedro Cyrino <pedroxcani@gmail.com>
2023-10-03 10:26:46 -0300

    Logica do Programa

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.senha = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Confirmar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome:";
+            // 
+            // nome
+            // 
+            this.nome.Location = new System.Drawing.Point(75, 52);
+            this.nome.Name = "nome";
+            this.nome.Size = new System.Drawing.Size(100, 20);
+            this.nome.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Senha:";
+            // 
+            // senha
+            // 
+            this.senha.BackColor = System.Drawing.SystemColors.Window;
+            this.senha.Location = new System.Drawing.Point(75, 103);
+            this.senha.Name = "senha";
+            this.senha.Size = new System.Drawing.Size(100, 20);
+            this.senha.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(329, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Procurar:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(385, 49);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(320, 100);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(231, 242);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(583, 501);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.senha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox senha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+// para criptografar a SENHA
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // para colocar no banco de dados
+            MySqlConnection conn = new MySqlConnection("Server=127.0.0.1;Uid=root;Pwd=usbw;Database=empresa;Persist Security Info=True;");
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO usuario(nome, senha) VALUES (@nome, @senha);", conn);
+                cmd.Parameters.AddWithValue("@nome", nome.Text);
+                cmd.Parameters.AddWithValue("@senha", CriarMD5Hash(senha.Text));
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            // para inserir uma quebra de linha
+            richTextBox1.AppendText(nome.Text + Environment.NewLine);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection("Server=127.0.0.1;Uid=root;Pwd=usbw;Database=empresa;Persist Security Info=True;");
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT nome FROM usuario WHERE nome LIKE @nome;", conn);
+                cmd.Parameters.AddWithValue("@nome", textBox3.Text + '%');
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                StringBuilder nomesEncontrados = new StringBuilder();
+
+                while (reader.Read())
+                {
+                    nomesEncontrados.AppendLine(reader["nome"].ToString());
+                }
+
+                if (nomesEncontrados.Length > 0)
+                {
+                    richTextBox1.Text = nomesEncontrados.ToString();
+                }
+                else
+                {
+                    richTextBox1.Text = "Nenhum resultado encontrado.";
+                }
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public string CriarMD5Hash(string input)
+        {
+            // Código para criar o hash MD5 permanece inalterado
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
